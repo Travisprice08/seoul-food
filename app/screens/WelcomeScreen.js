b5996ad67a2b8a397b3bb76e9bc3c7a159749d91
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2022-06-16 20:46:36 -0700

    Pressable
    Added pressable to customize button

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, ImageBackground, StyleSheet,Text, View } from 'react-native';
+import { Button, ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
 
 // function WelcomeScreen(props) {
     const WelcomeScreen = ({ navigation }) => {
@@ -10,12 +10,13 @@ import { Button, ImageBackground, StyleSheet,Text, View } from 'react-native';
         source={require("../assets/mom.jpg")}
         >
             <Text style = {styles.intro}>Welcome to Mom's Kitchen</Text>
-            <Button
+            <Pressable
                 style={styles.enterButton}
                 title="Lets cook some shit!"
                 onPress={() =>
-                    navigation.navigate('Main')}/>
-            {/* <View style={styles.enterButton}></View> */}
+                    navigation.navigate('Main')}>
+                <Text style={styles.buttonText}>Lets Cook Some Shit!</Text>
+            </Pressable>
         </ImageBackground>
     );
     };
@@ -28,8 +29,27 @@ import { Button, ImageBackground, StyleSheet,Text, View } from 'react-native';
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
+        justifyContent: "center",
+        // alignItems: "center",
+    },
+    buttonText: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
+    enterButton: {
+        position: 'absolute',
+        top: '65%',
+        left: '35%',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        paddingVertical: 22,
+        paddingHorizontal: 30,
+        borderRadius: 6,
+        elevation: 3,
+        backgroundColor: 'crimson',
     },
     intro: {
         color: 'white',
@@ -37,13 +57,8 @@ const styles = StyleSheet.create({
         fontWeight: '800',
         position: 'absolute',
         top: '25%',
-        
+        left: '25%',    
     },
-    enterButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: 'crimson',
-    }
 })
 
 export default WelcomeScreen;
